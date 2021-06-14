38026fcf9342834380c2a7bceb5d3e3d3bee33dc
Nebual <ben1120@gmail.com>
2021-06-14 03:17:26 -0700

    Wires now cleanup shortly after their ent is destroyed

--- code/entities/WirePortData.cs
+++ code/entities/WirePortData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eef1c22d2cf1efdd028292187577f37439f33c45998478a4bb1154e35a55109d
-size 662
+oid sha256:0f004aba0cf13c89d5b2b85c5bc605d6313346d62666ae6a9a926d1e6d76e10f
+size 2027
--- code/tools/Wiring.cs
+++ code/tools/Wiring.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b40ab98a0c92f9577398966e52ee7c5f4394a599ba0fd8e002f1504827a5ea4
-size 8989
+oid sha256:278184f2df696f9f6e5f41fc77da39b9a9db5fbf30a5a02be90458ad40f89fd7
+size 9034
