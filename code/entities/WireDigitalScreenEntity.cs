6851033f92f8c62cac5daf2de0a2d1e4657772fa
Nebual <ben1120@gmail.com>
2023-08-02 19:16:50 -0700

    Add Wire Digital Screen

---
+++ code/entities/WireDigitalScreenEntity.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+[Library( "ent_wiredigitalscreen", Title = "Wire Digital Screen" )]
+public partial class WireDigitalScreenEntity : Prop, IWireInputEntity
+{
+	WirePortData IWireEntity.WirePorts { get; } = new WirePortData();
+	[Net]
+	private float valueA { get; set; } = 0;
+
+	private string labelPrefix = "Wire Screen: ";
+	private DigitalScreenWorldPanel worldPanel;
+
+	public void WireInitialize()
+	{
+		this.RegisterInputHandler( "A", ( float value ) =>
+		{
+			valueA = float.Round( value, 1 );
+		} );
+	}
+
+	public override void ClientSpawn()
+	{
+		base.ClientSpawn();
+		worldPanel = new DigitalScreenWorldPanel
+		{
+			Transform = Transform,
+		};
+		worldPanel.Label.Style.Width = Model.Bounds.Size.y * 8.5f;
+		worldPanel.Label.Style.TextAlign = TextAlign.Center;
+		worldPanel.Label.Style.FontSize = Length.Pixels( Model.Bounds.Size.y * 1.25f );
+		worldPanel.Label.Style.FontColor = Color.White;
+	}
+
+	[GameEvent.Client.Frame]
+	protected void UpdatePanel()
+	{
+		if ( !Game.IsClient || !worldPanel.IsValid ) return;
+		var modelData = ScreenTransforms.GetValueOrDefault( Model.Name );
+		worldPanel.Position = Transform.PointToWorld( modelData.Position );
+		worldPanel.Rotation = Rotation.RotateAroundAxis( modelData.Rotation.Right, -modelData.Rotation.w );
+		worldPanel.Label.Text = labelPrefix + "\n" + valueA;
+	}
+
+	[Event( "spawnlists.initialize" )]
+	public static void SpawnlistsInitialize()
+	{
+		ModelSelector.AddToSpawnlist( "screen", new string[] {
+			Cloud.Asset("https://asset.party/baik/flatscreen_tv"),
+			Cloud.Asset("https://asset.party/eurorp/monitor"),
+		} );
+	}
+
+	public static readonly Dictionary<string, Transform> ScreenTransforms = new()
+	{
+		["models/television/flatscreen_tv.vmdl"] = new Transform( new Vector3( 13, 3, 1.75f ), new Rotation( new Vector3( 0, 1, 0 ), -90 ) ),
+		["models/others/monitor/monitor.vmdl"] = new Transform( new Vector3( -0.5f, 9, 1.25f ), Rotation.Identity ),
+	};
+}
+
+
+class DigitalScreenWorldPanel : WorldPanel
+{
+	public Label Label;
+	public DigitalScreenWorldPanel()
+	{
+		Label = Add.Label( "Wire Screen: \n0", "ds-text" );
+	}
+}
---
+++ code/tools/DigitalScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Sandbox.Tools
+{
+	[Library( "tool_wiredigitalscreen", Title = "Wire Digital Screen", Description = "Create a Wire Digital Screen for displaying numbers", Group = "construction" )]
+	public partial class DigitalScreenTool : BaseWireTool
+	{
+		[ConVar.ClientData( "tool_wiredigitalscreen_model" )]
+		public static string _ { get; set; } = "models/wirebox/katlatze/apc.vmdl";
+
+		protected override Type GetEntityType()
+		{
+			return typeof( WireDigitalScreenEntity );
+		}
+		protected override ModelEntity SpawnEntity( TraceResult tr )
+		{
+			return new WireDigitalScreenEntity
+			{
+				Position = tr.EndPosition,
+				Rotation = Rotation.LookAt( tr.Normal, tr.Direction ) * Rotation.From( new Angles( 90, 0, 0 ) )
+			};
+		}
+		protected override string[] GetSpawnLists()
+		{
+			return new string[] { "screen", "controller" };
+		}
+	}
+}
