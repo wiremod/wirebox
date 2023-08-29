c219ef7e61fc7e84568f8ff6269493fbe8284ca5
Nebual <ben1120@gmail.com>
2023-08-29 02:10:56 -0700

    Add Forcer + Ranger tool panels

--- code/tools/Forcer.cs
+++ code/tools/Forcer.cs
@@ -6,6 +6,9 @@ namespace Sandbox.Tools
 	{
 		[ConVar.ClientData( "tool_wireforcer_model" )]
 		public static string _ { get; set; } = "models/wirebox/katlatze/apc.vmdl";
+		[ConVar.ClientData( "tool_wireforcer_length" )]
+		public static float _2 { get; set; } = 100f;
+
 		protected override Type GetEntityType()
 		{
 			return typeof( WireRangerEntity );
@@ -15,12 +18,22 @@ namespace Sandbox.Tools
 			return new WireForcerEntity
 			{
 				Position = tr.EndPosition,
-				Rotation = Rotation.LookAt( tr.Normal, tr.Direction ) * Rotation.From( new Angles( 90, 0, 0 ) )
+				Rotation = Rotation.LookAt( tr.Normal, tr.Direction ) * Rotation.From( new Angles( 90, 0, 0 ) ),
+				Length = float.Parse( GetConvarValue( "tool_wireforcer_length" ) ),
 			};
 		}
 		protected override string[] GetSpawnLists()
 		{
 			return new string[] { "ranger", "controller" };
+		}
+
+		public override void CreateToolPanel()
+		{
+			if ( Game.IsClient )
+			{
+				var toolConfigUi = new ForcerToolConfig();
+				SpawnMenu.Instance?.ToolPanel?.AddChild( toolConfigUi );
+			}
 		}
 	}
 }
--- code/tools/Ranger.cs
+++ code/tools/Ranger.cs
@@ -6,6 +6,11 @@ namespace Sandbox.Tools
 	{
 		[ConVar.ClientData( "tool_wireranger_model" )]
 		public static string _ { get; set; } = "models/wirebox/katlatze/apc.vmdl";
+		[ConVar.ClientData( "tool_wireranger_length" )]
+		public static float _2 { get; set; } = 100f;
+		[ConVar.ClientData( "tool_wireranger_defaultzero" )]
+		public static bool _3 { get; set; } = true;
+
 		protected override Type GetEntityType()
 		{
 			return typeof( WireRangerEntity );
@@ -15,12 +20,23 @@ namespace Sandbox.Tools
 			return new WireRangerEntity
 			{
 				Position = tr.EndPosition,
-				Rotation = Rotation.LookAt( tr.Normal, tr.Direction ) * Rotation.From( new Angles( 90, 0, 0 ) )
+				Rotation = Rotation.LookAt( tr.Normal, tr.Direction ) * Rotation.From( new Angles( 90, 0, 0 ) ),
+				Length = float.Parse( GetConvarValue( "tool_wireranger_length" ) ),
+				DefaultZero = GetConvarValue( "tool_wireranger_defaultzero" ) != "0",
 			};
 		}
 		protected override string[] GetSpawnLists()
 		{
 			return new string[] { "ranger", "controller" };
+		}
+
+		public override void CreateToolPanel()
+		{
+			if ( Game.IsClient )
+			{
+				var toolConfigUi = new RangerToolConfig();
+				SpawnMenu.Instance?.ToolPanel?.AddChild( toolConfigUi );
+			}
 		}
 	}
 }
